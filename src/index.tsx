05a269afd3a12bb4fbc23f475b93e313f4579c24
RodrigoCortezDev <59732403+RodrigoCortezDev@users.noreply.github.com>
2020-05-29 00:45:45 -0300

    acertos

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { ConfigProvider } from "antd";
-import ptBr from "antd/es/locale/pt_BR";
-import "antd/dist/antd.css";
-import "moment/locale/pt-br";
-import moment from "moment";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ConfigProvider } from 'antd';
+import ptBr from 'antd/es/locale/pt_BR';
+import 'antd/dist/antd.css';
+import 'moment/locale/pt-br';
+import moment from 'moment';
+import App from './App';
 
-moment.locale("pt-BR");
+moment.locale('pt-BR');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ConfigProvider locale={ptBr}>
-      <App />
-    </ConfigProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <ConfigProvider locale={ptBr}>
+            <App />
+        </ConfigProvider>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
